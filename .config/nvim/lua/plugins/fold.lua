a93f2b1bdc8773a9087e9e70a33671be8f94bafa
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2024-10-26 14:08:05 +0500

    Disabled Plugins

--- .config/nvim/lua/plugins/fold.lua
+++ .config/nvim/lua/plugins/fold.lua
@@ -1,7 +1,13 @@
+-- TODO: Make this work, currently does nothing.
 return {
   'jghauser/fold-cycle.nvim',
+  enabled = false,
   config = function()
     require("fold-cycle").setup({
+      open_if_max_closed = true, -- closing a fully closed fold will open it
+      close_if_max_opened = true,
+      softwrap_movement_fix = true,
+      -- keymaps
       vim.keymap.set('n', '<leader>do',
         function() return require('fold-cycle').open() end,
         { silent = true, desc = 'Fold-cycle: open folds' }),
@@ -10,6 +16,7 @@ return {
         { silent = true, desc = 'Fold-cycle: close folds' }),
       vim.keymap.set('n', '<leader>dc',
         function() return require('fold-cycle').close_all() end,
-        { remap = true, silent = true, desc = 'Fold-cycle: close all folds' }) })
+        { remap = true, silent = true, desc = 'Fold-cycle: close all folds' })
+    })
   end
 }
