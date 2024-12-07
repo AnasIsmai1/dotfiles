42bdcd4842c21646e2db5a11789b09626f1e6add
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2024-12-07 08:53:10 +0500

    Added codeium ai

--- .config/nvim/lua/plugins/ai.lua
+++ .config/nvim/lua/plugins/ai.lua
@@ -1,10 +1,12 @@
 return {
-  'Exafunction/codeium.nvim',
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "hrsh7th/nvim-cmp",
-  },
-  config = function()
-    require("codeium").setup({})
-  end
+    'Exafunction/codeium.nvim',
+    enabled = false,
+    event = { "InsertEnter", "BufReadPre" },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
+    },
+    config = function()
+        require("codeium").setup({})
+    end
 }
