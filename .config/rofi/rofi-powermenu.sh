19ce12a40a0b70683165174f2854a2821dca11e0
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2024-12-07 08:52:32 +0500

    Changed menu for shutdown

--- .config/rofi/rofi-powermenu.sh
+++ .config/rofi/rofi-powermenu.sh
@@ -5,14 +5,20 @@ chosen=$(printf "⏻ Power Off\n󰜉 Restart\n  Sleep\n󰈆  Exit" | rofi -dm
 echo "Chosen option: $chosen"
 
 case "$chosen" in
-	"⏻ Power Off")
-		shutdown -h now ;;
-	"󰜉 Restart")
-		reboot ;;
-	"  Sleep")
-		i3lock && systemctl suspend ;;
-  "󰈆  Exit")
-    rofi -show window;;
-	*)
-		echo "Invalid option selected: $chosen";	exit 1 ;;
-esac 
+"⏻ Power Off")
+  shutdown -h now
+  ;;
+"󰜉 Restart")
+  reboot
+  ;;
+"  Sleep")
+  i3lock - c 000000 && systemctl suspend
+  ;;
+"󰈆  Exit")
+  rofi -show window
+  ;;
+*)
+  echo "Invalid option selected: $chosen"
+  exit 1
+  ;;
+esac
