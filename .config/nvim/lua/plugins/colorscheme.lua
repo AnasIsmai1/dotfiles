a304b5bcbf2409daeaa9aeeb6b9e9d72cc39e807
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2024-12-07 08:54:17 +0500

    Enabled catppuccin theme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -9,6 +9,38 @@ return {
   --      }
   -- 	end,
   -- },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        transparent_background = false,
+        styles = {
+          comments = { "italic" },
+          conditionals = { "italic" }
+        },
+        dim_inactive = {
+          enabled = true,    -- dims the background color of inactive window
+          shade = "dark",
+          percentage = 0.25, -- percentage of the shade to apply to the inactive window
+        },
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          nvimtree = true,
+          treesitter = true,
+          notify = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "",
+          },
+          -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+        },
+      })
+      vim.cmd.colorscheme "catppuccin-mocha"
+    end,
+  },
   -- {
   -- 	"AlexvZyl/nordic.nvim",
   -- 	lazy = false,
@@ -17,13 +49,13 @@ return {
   -- 		require("nordic").load()
   -- 	end,
   -- 	},
-  {
-    "sho-87/kanagawa-paper.nvim",
-    event = "vimenter",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-  },
+  -- {
+  --   "sho-87/kanagawa-paper.nvim",
+  --   event = "vimenter",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  -- },
   -- {
   -- 	"tiagovla/tokyodark.nvim",
   -- 	opts = {
@@ -34,19 +66,19 @@ return {
   -- 		vim.cmd([[colorscheme tokyodark]])
   -- 	end,
   -- },
-  {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-      require("tokyonight").setup({
-        style = "night",
-        on_colors = function(colors)
-          colors.bg_dark = "#011423"
-        end,
-      })
-      vim.cmd("colorscheme tokyonight")
-    end,
-  },
+  -- {
+  --   "folke/tokyonight.nvim",
+  --   lazy = false,
+  --   priority = 1000,
+  --   opts = {},
+  --   config = function()
+  --     require("tokyonight").setup({
+  --       style = "night",
+  --       on_colors = function(colors)
+  --         colors.bg_dark = "#011423"
+  --       end,
+  --     })
+  --     vim.cmd("colorscheme tokyonight")
+  --   end,
+  -- },
 }
