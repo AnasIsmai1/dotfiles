7926df735b7f40a14154f11d41ae0396959ca018
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2024-12-07 08:52:23 +0500

    Added sessionizer plugin for tmux

--- .config/sesh/preview.sh
+++ .config/sesh/preview.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-tmux list-panes -a -F '#S:#I.#P #{pane_current_command}' | \
-fzf --prompt="⚡  " \
-    --ansi \
-    --header="S+up => Scroll-up, S-down => Scroll-down" \
-    --preview='tmux capture-pane -p -t {1}' --bind shift-up:preview-up,shift-down:preview-down
+tmux list-panes -a -F '#S:#I.#P #{pane_current_command}' |
+  fzf --prompt="⚡  " \
+    --ansi --reverse --no-sort --cycle --tiebreak=index \
+    --pointer=" " --marker=" " --color='pointer:#FF204E,marker:#FF204E' \
+    --header="S+up => Scroll-up, S-down => Scroll-down, Ctrl-\ => Toggle-hidden" \
+    --preview='tmux capture-pane -p -t {1}' \
+    --preview-window='bottom:60%' \
+    --bind shift-up:preview-up,shift-down:preview-down \
+    --bind 'ctrl-\:change-preview-window(hidden|)'
--- .config/sesh/sessionizer.sh
+++ .config/sesh/sessionizer.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 export PATH="$HOME/.cargo/bin:$PATH"
 $HOME/go/bin/sesh connect $(
-$HOME/go/bin/sesh list -tz --icons | fzf-tmux -p 55%,60% \
-  --ansi --no-sort --border-label ' sesh ' --prompt '⚡  ' \
-  --header '  ^a all ^t tmux ^g config ^x zoxide ^d tmux kill ^f find ^r rename ^p preview ' \
-  --bind 'tab:down,btab:up' \
-  --bind 'ctrl-a:change-prompt(⚡  )+reload($HOME/go/bin/sesh list --icons)' \
-  --bind 'ctrl-t:change-prompt(🪟 )+reload($HOME/go/bin/sesh list -t --i)' \
-  --bind 'ctrl-g:change-prompt(⚙️  )+reload($HOME/go/bin/sesh list -c --icons)' \
-  --bind 'ctrl-x:change-prompt(📁  )+reload($HOME/go/bin/sesh list -z --i)' \
-  --bind 'ctrl-f:change-prompt(🔎 )+reload(fdfind -H -d 2 -t d -E .Trash . ~)' \
-  --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload($HOME/go/bin/sesh list --i)' \
-  --bind 'ctrl-r:execute(current=$(tmux display-message -p {2..}); name=$(echo $current | fzf --print-query --prompt="New name (current: $current): ");    tmux rename-session -t {2..} $name )+reload($HOME/go/bin/sesh list --i)' \
-  --bind 'ctrl-p:execute(~/.config/sesh/preview.sh)'
+  $HOME/go/bin/sesh list -tz --icons | fzf-tmux -p 75%,65% \
+    --ansi --reverse --no-sort --border-label ' sesh ' --prompt='⚡  ' \
+    --header '  ^a all ^t tmux ^g config ^x zoxide ^d tmux kill ^f find ^r rename ^p preview ' \
+    --pointer=" " --marker=" " --color='pointer:#FF204E,marker:#FF204E' \
+    --bind 'tab:down,btab:up' \
+    --bind 'ctrl-a:change-prompt(⚡  )+reload($HOME/go/bin/sesh list --icons)' \
+    --bind 'ctrl-t:change-prompt(🪟 )+reload($HOME/go/bin/sesh list -t --i)' \
+    --bind 'ctrl-g:change-prompt(⚙️  )+reload($HOME/go/bin/sesh list -c --icons)' \
+    --bind 'ctrl-x:change-prompt(📁  )+reload($HOME/go/bin/sesh list -z --i)' \
+    --bind 'ctrl-f:change-prompt(🔎 )+reload(fdfind -H -d 2 -t d -E .Trash . ~)' \
+    --bind 'ctrl-p:execute(~/.config/sesh/preview.sh)' \
+    --bind 'ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload($HOME/go/bin/sesh list --i)' \
+    --bind 'ctrl-r:execute(current=$(tmux display-message -p {}); name=$( echo "Input your new Name" | fzf --header "Current Name: $current"  --ansi --reverse --print-query --prompt="New name: ");    tmux rename-session -t {2..} $name )+reload($HOME/go/bin/sesh list --i)'
 )
