d3f27020f9d800ca1927f5f7ffe3622d61473e92
AnasIsmai1 <174011704+AnasIsmai1@users.noreply.github.com>
2025-02-02 22:57:36 +0500

    update markdown config

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,12 +1,13 @@
 return {
-  {
-    'MeanderingProgrammer/markdown.nvim',
-    main = 'render-markdown',
-    opts = {},
-    name = 'render-markdown', -- only needed if you have another plugin named markdown.nvim
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-tree/nvim-web-devicons'
+    {
+        'MeanderingProgrammer/markdown.nvim',
+        enabled = true,
+        main = 'render-markdown',
+        opts = {},
+        name = 'render-markdown', -- only needed if you have another plugin named markdown.nvim
+        dependencies = {
+            'nvim-treesitter/nvim-treesitter',
+            'nvim-tree/nvim-web-devicons'
+        },
     },
-  },
 }
